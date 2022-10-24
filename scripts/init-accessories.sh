db68654dd5e197b3be88e19005b840c4ab6cedad
hackboxguy <albert.david@gmail.com>
2022-10-24 12:12:25 +0200

    added init-accessories.sh script

---
+++ scripts/init-accessories.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#this script is used as an accessories initialization script after startup of aws-iot-pubsub-demo service.
+#on startup, initialize power-led based on slider-button position of GLMT300NV2
+TESTBTNST=$(cat /sys/kernel/debug/gpio |grep gpio-480|awk '{print $6}')
+if [ "$TESTBTNST" == "hi" ]; then
+       /usr/sbin/led-power.sh off
+else
+       /usr/sbin/led-power.sh on
+fi
+return 0
