48ed990b369dfee15494b272779f2d9e90a3d02f
hackboxguy <albert.david@gmail.com>
2022-10-28 18:48:00 +0200

    removed subscribe-topic and subscribe-handler as mandatory args

--- luci-ui/luci/model/cbi/awsiot_cbi/awsiot-srvice-settings.lua
+++ luci-ui/luci/model/cbi/awsiot_cbi/awsiot-srvice-settings.lua
@@ -6,7 +6,7 @@ c = d:option(Value, "topic", "Publish Topic"); c.optional=false; c.rmempty = fal
 e = d:option(Value, "interval", "Publish Inverval Sec"); e.optional=false; e.rmempty = false;
 f = d:option(Value, "count", "Publish Count"); f.optional=false; f.rmempty = false;
 h = d:option(Value, "message", "Publish Message"); h.optional=false; h.rmempty = false;
-i = d:option(Value, "subtopic", "Subscribe Topic"); i.optional=false; i.rmempty = false;
-j = d:option(Value, "subtopichandler", "Subscribe Topic Handler"); j.optional=false; j.rmempty = false;
+i = d:option(Value, "subtopic", "Subscribe Topic"); i.optional=false; i.rmempty = true;
+j = d:option(Value, "subtopichandler", "Subscribe Topic Handler"); j.optional=false; j.rmempty = true;
 g = d:option(Value, "verbosity", "Server Logging Verbosity"); g.optional=false; g.rmempty = false;
 return m
